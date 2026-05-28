e947ecf2036905ed79303b8d6a53e80eda65b707
Laurensius <laurensiusdatukadingi@gmail.com>
2026-05-28 09:45:56 +0800

    konfigurasi vercel6

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", apiRoutes);
 
 // SPA fallback
-app.get("*", (req, res) => {
+app.get(/^((?!\/api).)*$/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
