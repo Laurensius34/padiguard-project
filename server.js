0caf1231e105dcd419929555dfb83141df2c1a2e
Laurensius <laurensiusdatukadingi@gmail.com>
2026-05-28 08:48:17 +0800

    konfigurasi vercel3

--- server.js
+++ server.js
@@ -25,8 +25,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`PadiGuard running successfully on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`PadiGuard running successfully on http://localhost:${PORT}`);
+  });
+}
 
 export default app;
